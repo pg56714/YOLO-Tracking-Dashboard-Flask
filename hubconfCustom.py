d6ea615480592cedfee7c819fe1ab1320f146f6c
HONG,HAO-SYUAN <pg785632541@gmail.com>
2024-09-01 17:44:40 +0800

    update formatter

--- hubconfCustom.py
+++ hubconfCustom.py
@@ -6,17 +6,90 @@ import time
 import random
 
 
-def video_detection(path_x='', conf_=0.25):
+def video_detection(path_x="", conf_=0.25):
     total_detections = 0
-    names = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light',
-             'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
-             'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
-             'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard',
-             'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
-             'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch',
-             'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
-             'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear',
-             'hair drier', 'toothbrush']
+    names = [
+        "person",
+        "bicycle",
+        "car",
+        "motorcycle",
+        "airplane",
+        "bus",
+        "train",
+        "truck",
+        "boat",
+        "traffic light",
+        "fire hydrant",
+        "stop sign",
+        "parking meter",
+        "bench",
+        "bird",
+        "cat",
+        "dog",
+        "horse",
+        "sheep",
+        "cow",
+        "elephant",
+        "bear",
+        "zebra",
+        "giraffe",
+        "backpack",
+        "umbrella",
+        "handbag",
+        "tie",
+        "suitcase",
+        "frisbee",
+        "skis",
+        "snowboard",
+        "sports ball",
+        "kite",
+        "baseball bat",
+        "baseball glove",
+        "skateboard",
+        "surfboard",
+        "tennis racket",
+        "bottle",
+        "wine glass",
+        "cup",
+        "fork",
+        "knife",
+        "spoon",
+        "bowl",
+        "banana",
+        "apple",
+        "sandwich",
+        "orange",
+        "broccoli",
+        "carrot",
+        "hot dog",
+        "pizza",
+        "donut",
+        "cake",
+        "chair",
+        "couch",
+        "potted plant",
+        "bed",
+        "dining table",
+        "toilet",
+        "tv",
+        "laptop",
+        "mouse",
+        "remote",
+        "keyboard",
+        "cell phone",
+        "microwave",
+        "oven",
+        "toaster",
+        "sink",
+        "refrigerator",
+        "book",
+        "clock",
+        "vase",
+        "scissors",
+        "teddy bear",
+        "hair drier",
+        "toothbrush",
+    ]
     colors = [[random.randint(0, 255) for _ in range(3)] for _ in names]
     filter_classess = None
 
@@ -27,7 +100,7 @@ def video_detection(path_x='', conf_=0.25):
         tracker=asone.BYTETRACK,
         detector=asone.YOLOV8N_PYTORCH,
         weights=None,
-        use_cuda=False
+        use_cuda=False,
     )
 
     start_time = time.time()
@@ -44,9 +117,8 @@ def video_detection(path_x='', conf_=0.25):
         ret, img0 = video.read()
         fps_x = int((j + 1) / (time.time() - start_time))
         if ret:
-            detected = dt_obj.detecter(source=img0,
-                                       conf_thres=conf_, iou_thres=0.45)
-            
+            detected = dt_obj.detecter(source=img0, conf_thres=conf_, iou_thres=0.45)
+
             bboxes_xyxy = detected.dets.bbox
             scores = detected.dets.score
             class_ids = detected.dets.class_ids
@@ -54,9 +126,14 @@ def video_detection(path_x='', conf_=0.25):
             for i in range(len(bboxes_xyxy)):
                 total_detections += 1
                 box = bboxes_xyxy[i]
-                label = f'{names[int(class_ids[i])]} {scores[i]:.2f}'
-                draw_ui_box(box, img0, label=label,
-                            color=colors[int(class_ids[i])], line_thickness=3)
+                label = f"{names[int(class_ids[i])]} {scores[i]:.2f}"
+                draw_ui_box(
+                    box,
+                    img0,
+                    label=label,
+                    color=colors[int(class_ids[i])],
+                    line_thickness=3,
+                )
 
             yield img0, fps_x, img0.shape, total_detections
 
